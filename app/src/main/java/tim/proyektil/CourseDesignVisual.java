c0825a15414f6d6f4a4a486d08e67b5650d3000b
kevinardhana096 <kevinardhana096@gmail.com>
2024-06-01 23:10:31 +0800

    Kumpul Tugas

--- app/src/main/java/tim/proyektil/CourseDesignVisual.java
+++ app/src/main/java/tim/proyektil/CourseDesignVisual.java
@@ -7,16 +7,13 @@ public class CourseDesignVisual extends BaseCourseScene {
 
     public CourseDesignVisual(EventHandler<javafx.event.ActionEvent> backButtonHandler, Stage primaryStage) {
         super("Design Visual",
-              new String[]{
-                  "/image/Computer Graphic.png",
-                  "/image/Introdution Visual.png",
-                  "/image/Fundamental .png" 
-              },
-              new String[]{
-                  "/pdf/Computer Graphics Through OpenGL_ From Theory to Experiments ( PDFDrive ).pdf",
-                  "/pdf/Introduction to Visual Computing_ Core Concepts in Computer Vision, Graphics, and Image Processing ( PDFDrive ).pdf",
-                  "/pdf/Fundamentals of Computer Graphics, Fourth Edition ( PDFDrive ).pdf" // Add the third PDF path
-              },
-              backButtonHandler, primaryStage);
+                new String[] {
+                        "/image/Fundamental .png"
+                },
+                new String[] {
+                        "/pdf/Fundamentals of Computer Graphics, Fourth Edition ( PDFDrive ).pdf" // Add the third PDF
+                                                                                                  // path
+                },
+                backButtonHandler, primaryStage);
     }
 }
